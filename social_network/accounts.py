35a7b53927352e47144b00ec9abda45eddf1e343
RyanWerth <ryanwerth@pacbell.net>
2018-03-19 19:11:09 +0000

    Finished the project

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,22 @@
 
 class User(object):
     def __init__(self, first_name, last_name, email):
-        pass
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.following = []
+        self.posts = []
 
     def add_post(self, post):
-        pass
+        self.posts.append(post)
 
     def get_timeline(self):
-        pass
+        timeline = []
+        for user in self.following:
+            for post in user.posts:
+                timeline.append(post)
+        timeline.sort(key=lambda x:x.timestamp)
+        return timeline
 
     def follow(self, other):
-        pass
+        self.following.append(other)
--- social_network/posts.py
+++ social_network/posts.py
@@ -1,33 +1,43 @@
 from datetime import datetime
 
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
+class Post(object):
+     def __init__(self, text, timestamp=None):
+         self.text = text
+         self.timestamp = timestamp
+         self.user = None
+
+     def set_user(self, user):
+         self.user = user
 
 
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class TextPost(Post):  # Inherit properly
+     def __str__(self):
+         return '@{firstname} {lastname}: "{text}"\n\t{weekday}, {month} {day}, {year}'.format(
+              firstname=self.user.first_name, lastname=self.user.last_name, text=self.text, weekday=self.timestamp.strftime("%A"), month = self.timestamp.strftime("%b"),
+              day = self.timestamp.day, year = self.timestamp.year)
 
 
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class PicturePost(Post):  # Inherit properly
+     def __init__(self, text, image_url, timestamp=None):
+         super(PicturePost, self).__init__(text, timestamp)
+         self.image_url = image_url
+
+     def __str__(self):
+         return '@{firstname} {lastname}: "{text}"\n\t{url}\n\t{weekday}, {month} {day}, {year}'.format(
+              firstname=self.user.first_name, lastname=self.user.last_name, text=self.text, url = self.image_url, weekday=self.timestamp.strftime("%A"), 
+              month = self.timestamp.strftime("%b"),
+              day = self.timestamp.day, year = self.timestamp.year)
 
 
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class CheckInPost(Post):  # Inherit properly
+     def __init__(self, text, latitude, longitude, timestamp=None):
+         super(CheckInPost, self).__init__(text, timestamp)
+         self.latitude = latitude
+         self.longitude = longitude
+
+     def __str__(self):
+         return '@{firstname} Checked In: "{text}"\n\t{lat}, {lon}\n\t{weekday}, {month} {day}, {year}'.format(
+              firstname=self.user.first_name, text=self.text, lat = self.latitude, lon = self.longitude, 
+              weekday=self.timestamp.strftime("%A"), month = self.timestamp.strftime("%b"),
+              day = self.timestamp.day, year = self.timestamp.year)
